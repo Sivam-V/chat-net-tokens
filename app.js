072095ade79d78f580f4fcf123fc2d085af2e3ee
Sivam-V <sivamvenugopal2005@gmail.com>
2025-07-24 18:43:44 +0530

    Create app.js
    app.js that is created and used to turn on the browser and mongodb server also at the same time

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+        app.listen(process.env.PORT, () => {
+            console.log(`Server running on port ${process.env.PORT}`);
+        });
+    })
+    .catch(err => console.error(err));
